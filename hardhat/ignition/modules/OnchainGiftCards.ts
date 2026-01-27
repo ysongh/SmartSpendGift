903b001b523179efa361d96e24ee7f50544acc5c
You Song Hou <ysongweb3@gmail.com>
2026-01-27 00:15:52 -0500

    feat: update deployment script to deploy mock USDC contract

--- hardhat/ignition/modules/OnchainGiftCards.ts
+++ hardhat/ignition/modules/OnchainGiftCards.ts
@@ -1,7 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("OnchainGiftCardsModule", (m) => {
-  const onchainGiftCards = m.contract("OnchainGiftCards");
+  const mockUSDC = m.contract("MockUSDC", []);
+  const onchainGiftCards = m.contract("OnchainGiftCards", [mockUSDC]);
 
   return { onchainGiftCards };
 });
